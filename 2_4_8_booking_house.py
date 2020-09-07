3b33db4228acb47e65edc60703196fd63809be99
Likhachev <grigoriy.likhachev@gmail.com>
2020-09-07 22:30:10 +0300

    Add task 2_4_8 autotest booking house

---
+++ 2_4_8_booking_house.py
@@ -0,0 +1,30 @@
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    WebDriverWait(browser,5).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    browser.find_element_by_id("book").click()
+
+    browser.find_element_by_id('answer').send_keys(calc(browser.find_element_by_id('input_value').text))
+    browser.find_element_by_id("solve").click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
